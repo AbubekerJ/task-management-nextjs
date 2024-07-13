d56dc8345b7e638c365265613a4fbc1494cf6f25
AbubekerJ <abubeker.jemal03@gmail.com>
2024-07-13 16:25:38 +0100

    Correct Dashbord styling

--- client/src/app/dashbord/page.jsx
+++ client/src/app/dashbord/page.jsx
@@ -116,68 +116,68 @@ function Dashboard() {
 
   return (
     <>
-    <div className="min-h-screen bg-gray-100 p-6">
-      <div className="container mx-auto">
-        <div className="flex justify-between items-center mb-6">
-          <div className="flex items-center space-x-2">
-            <span className="text-2xl">👋</span>
-            <h1 className="text-3xl font-bold text-blue-700">{`Hey, ${currentUser.username}`}</h1>
+      <div className="min-h-screen bg-gray-100 p-6">
+        <div className="container mx-auto">
+          <div className="flex justify-between items-center mb-6">
+            <div className="flex items-center space-x-2">
+              <span  className="text-2xl">👋</span>
+              <h1 className="text-2xl font-bold">{`Hey, ${currentUser.username}`}</h1>
+            </div>
+            <div className='flex flex-col gap-1 sm:flex-row gap-2'>
+              <button
+                onClick={() => setShowTaskForm(true)}
+                className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition duration-300 "
+              >
+                Add Task
+              </button>
+              <button
+                onClick={handleSignOut}
+                className="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700 transition duration-300"
+              >
+                Sign Out
+              </button>
+            </div>
           </div>
-          <div className="flex flex-col gap-1 sm:flex-row sm:gap-2">
-            <button
-              onClick={() => setShowTaskForm(true)}
-              className="bg-gradient-to-r from-blue-500 to-blue-600 text-white px-4 py-2 rounded-lg hover:from-blue-600 hover:to-blue-700 transition duration-300"
+          
+          <div className="flex justify-end mb-4">
+            <select
+              value={sortOrder}
+              onChange={(e) => setSortOrder(e.target.value)}
+              className="bg-white border border-gray-300 rounded-lg px-4 py-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
             >
-              Add Task
+              <option value="latest">Latest</option>
+              <option value="oldest">Oldest</option>
+            </select>
+          </div>
+
+          <div className="flex justify-center space-x-4 mb-4">
+            <button onClick={handleCompletedTask} className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 transition duration-300">
+              Completed Tasks
             </button>
-            <button
-              onClick={handleSignOut}
-              className="bg-gradient-to-r from-red-500 to-red-600 text-white px-4 py-2 rounded-lg hover:from-red-600 hover:to-red-700 transition duration-300"
-            >
-              Sign Out
+            <button onClick={handlePendingTask} className="bg-yellow-500 text-white px-4 py-2 rounded hover:bg-yellow-600 transition duration-300">
+              Pending Tasks
             </button>
           </div>
-        </div>
 
-        <div className="flex justify-end mb-4">
-          <select
-            value={sortOrder}
-            onChange={(e) => setSortOrder(e.target.value)}
-            className="bg-white border border-gray-300 rounded-lg px-4 py-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-          >
-            <option value="latest">Latest</option>
-            <option value="oldest">Oldest</option>
-          </select>
+          {loading ? (
+            <p>Loading tasks...</p>
+          ) : error ? (
+            <p className="text-red-500">Error: {error}</p>
+          ) : tasks.length === 0 ? (
+            <p>No tasks available</p>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+              {tasks.map((task) => (
+                <Tasks task={task} key={task.id} fetchData={fetchData} filteredUsers={filteredUsers} />
+              ))}
+            </div>
+          )}
         </div>
-
-        <div className="flex justify-center space-x-4 mb-4">
-          <button onClick={handleCompletedTask} className="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600 transition duration-300">
-            Completed Tasks
-          </button>
-          <button onClick={handlePendingTask} className="bg-yellow-500 text-white px-4 py-2 rounded-lg hover:bg-yellow-600 transition duration-300">
-            Pending Tasks
-          </button>
-        </div>
-
-        {loading ? (
-          <p>Loading tasks...</p>
-        ) : error ? (
-          <p className="text-red-500">Error: {error}</p>
-        ) : tasks.length === 0 ? (
-          <p>No tasks available</p>
-        ) : (
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {tasks.map((task) => (
-              <Tasks task={task} key={task.id} fetchData={fetchData} filteredUsers={filteredUsers} />
-            ))}
-          </div>
-        )}
       </div>
-     
-    </div>
-    {showTaskForm && <TaskForm mode={'create'} setShowTaskForm={setShowTaskForm} fetchData={fetchData} filteredUsers={filteredUsers} />}
+
+      {showTaskForm && <TaskForm mode={'create'} setShowTaskForm={setShowTaskForm} fetchData={fetchData}filteredUsers={filteredUsers} />}
     </>
   );
 }
 
-export default Dashboard;
+export default Dashboard
