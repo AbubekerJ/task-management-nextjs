ae074f6fdedd94aace15f5de1b777ce0352da5d6
AbubekerJ <abubeker.jemal03@gmail.com>
2024-08-03 10:27:10 +0100

    Update protect middleware

--- client/src/middleware.js
+++ client/src/middleware.js
@@ -2,15 +2,16 @@ import { NextResponse } from 'next/server'
 
  
 
-export function middleware(request) {
+export async function middleware(request) {
     
-  const token = request.cookies.get('access_token')?.value;
+  // const token = request.cookies.get('access_token');
+
   
-    console.log(token)
-    if(!token){
-        return NextResponse.redirect(new URL('/signin', request.url))
-    }
-    return NextResponse.next()
+  //   console.log(token)
+  //   if(!token){
+  //       return NextResponse.redirect(new URL('/signin', request.url))
+  //   }
+  //   return NextResponse.next()
   
 }
  
